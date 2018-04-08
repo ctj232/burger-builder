de4db2fdece8978a090b7c8c674716b5a7163096
Travis Johnson <cjohnso0@mymail.mines.edu>
2018-04-08 14:47:37 -0600

    Copy fancier spinner CSS from udemy zips

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Spinner.css';
 
 const spinner = () => (
-  <div className={styles.Loader}></div>
+  <div className={styles.Loader}>Loading...</div>
 );
 
 export default spinner;
