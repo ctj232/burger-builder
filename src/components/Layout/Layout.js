3973b534464e6c4069dad18a3d77e0f4aefa5f4b
Travis Johnson <cjohnso0@mymail.mines.edu>
2018-04-08 09:03:09 -0600

    Make Layout a stateful component and add handlers for managing state of Toolbar/Sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <div className={styles.Header}>Sidebar</div>
-    <main className={styles.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class layout extends Component {
+  state = {
+    showSideDrawer: false,
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  sideDrawerOpenHandler = () => {
+    this.setState({showSideDrawer: true})
+  }
+   
+  render () {
+    return (
+    <Aux>
+      <Toolbar toggle={this.sideDrawerOpenHandler}/>
+      <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+      <div className={styles.Header}>Sidebar</div>
+      <main className={styles.Content}>
+        {this.props.children}
+      </main>
+      </Aux>
+    )
+  }
+};
 
 export default layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <div onClick={props.toggle}>MENU</div>
     <div className={classes.Logo}><Logo /></div>
     <nav className={classes.DesktopOnly}>
       <NavigationItems/>
