b0e36b1376972d601a19bd6d3c4539687abe4da2
Travis Johnson <tjohnson@credera.com>
2018-04-10 13:33:33 -0600

    Add GET orders list from firebase backend and render Orders as a list of Order components

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
+const order = (props) => {
+	const ingredients = [];
+	for ( let ingrName in props.ingredients ) {
+		ingredients.push({name: ingrName, amt: props.ingredients[ingrName]})
+
+	}
+	const ingredientOutput = ingredients.map(
+		ig => {
+			return <span key={ig.name}>{ig.name} ({ig.amt})</span>;
+		}
+	);
+	return (
 	<div className={classes.Order}>
-		<p>Ingredients: {this.props.ingredients}</p>
-		<p>Price: <strong>{this.props.ingredients}</strong></p>
+		<p>Ingredients: {ingredientOutput}</p>
+		<p>Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong></p>
 	</div>
-);
+	);
+};
 
 export default order;
